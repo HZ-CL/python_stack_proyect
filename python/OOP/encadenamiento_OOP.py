8cb5f481cca72145ef591bab7a1a3b5ee8f02ca5
Hector Zambrano <hzambranoleal@gmail.com>
2021-02-03 12:59:32 -0300

    Python OOP encadenamiento

--- python/OOP/encadenamiento_OOP.py
+++ python/OOP/encadenamiento_OOP.py
@@ -7,17 +7,23 @@ class User:
     # metodo para depositos
     def make_deposit(self, amount): #amount es el argumento del monto del deposito
         self.account_balance += amount #la cuenta del usuario especifico(sefl.) aumenta por la cantidad
+        return self
 
     def make_withdrawal (self, amount):
         self.account_balance -= amount
+        return self
+
 
     def display_user_balance (self):
         print ("Usuario: " + self.name + "," , "Saldo: $" + str( self.account_balance))
+        return self
 
 
     def transfer_money (self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
+
 
 
 guido = User("Guido van Rossum", "guido@python.com")
@@ -25,34 +31,17 @@ monty = User("Monty Python", "monty@python.com")
 stev = User("Stev rosaroza", "steve@python.com")
 
 print("--Usuario 1--")
+guido.make_deposit(500).make_deposit(100).make_deposit(600).display_user_balance().make_withdrawal(200).display_user_balance()
 
-guido.make_deposit(500)#deposito 1
-guido.make_deposit(100)#deposito 2
-guido.make_deposit(600)#deposito 3
-guido.display_user_balance()#saldo 1200
-guido.make_withdrawal(200)#retiro 1
-guido.display_user_balance()#saldo 1000
-
+print()
 print("--Usuario 2--")
+monty.make_deposit(120).make_deposit(180).make_withdrawal(50).make_withdrawal(150).display_user_balance()
 
-
-monty.make_deposit(120)#deposito 1
-monty.make_deposit(180)#deposito 1
-monty.make_withdrawal(50)#retiro 1
-monty.make_withdrawal(150)#retiro 2
-monty.display_user_balance()#saldo 100
-
+print()
 print("--Usuario 3--")
+stev.make_deposit(600).make_withdrawal(100).make_withdrawal(140).make_withdrawal(160).display_user_balance()
 
-
-stev.make_deposit(600)#deposito 1
-stev.make_withdrawal(100)#retiro 1
-stev.make_withdrawal(140)#retiro 2
-stev.make_withdrawal(160)#retiro 3
-stev.display_user_balance()#saldo 200
-
+print()
 print("--Transferencia--")
-
-guido.transfer_money(stev,700)
-guido.display_user_balance()#saldo 300
-stev.display_user_balance()#saldo 900
+guido.transfer_money(stev,700).display_user_balance()
+stev.display_user_balance()
