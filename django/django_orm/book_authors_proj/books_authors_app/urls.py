8ee802f2596239c73d51fcf15f292064b1db8bdc
Hector Zambrano <hzambranoleal@gmail.com>
2021-02-27 14:35:24 -0300

    libros y autores, ORM asignar libros-autores

--- django/django_orm/book_authors_proj/books_authors_app/urls.py
+++ django/django_orm/book_authors_proj/books_authors_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.view_books),
     path('add_book', views.add_book),
     path('view_book/<id_book>', views.book_info),
-    path('view_book/asignar_autor/<id_book>', views.asignando_autor)
+    path('view_book/asignar_autor/<id_book>', views.asignando_autor),
+    path('view_authors', views.view_authors),
+    path('add_author', views.add_author),
+    path('author_info/<id_author>', views.author_info),
+    path('author_info/asignar_libro/<id_author>', views.asignando_libro)
+
 ]
--- django/django_orm/book_authors_proj/books_authors_app/views.py
+++ django/django_orm/book_authors_proj/books_authors_app/views.py
@@ -32,9 +32,41 @@ def book_info(request, id_book):
     return render(request, 'view_book.html', context)
 
 def asignando_autor(request, id_book):
-    id_autor = request.POST['id_autor'] #obtengo el id del autor para asignar al libro
-    autor_asig = Autor.objects.get(id=id_autor)
-    id_libro = Libro.objects.get(id=id_book)    #obtengo id del libro
-    autor_asig.libros.add(id_libro) # ORM para asiganar autor-libro
+    id_autor = request.POST['id_autor']         #obtengo el id del autor para asignar al libro
+    autor_asig = Autor.objects.get(id=id_autor) #ORM para traer el autor
+    id_libro = Libro.objects.get(id=id_book)    #ORM obtengo el libro
+    autor_asig.libros.add(id_libro)             # ORM para asiganar autor-libro
 
     return redirect(f'/view_book/{id_book}')
+
+def view_authors(request):
+    lis_authors = Autor.objects.all() #descargo la lista de autores desde base de datos
+    context = {
+        'lista' : lis_authors
+    }
+    return render(request, 'add_author.html', context)
+
+def add_author(request):
+    dato_nombre_form = request.POST['nombre_autor']         #extraigo el dato nombre
+    dato_apellido_form = request.POST['apellido_autor']     #extraigo el dato apellido
+    dato_notas_form = request.POST['notas_autor']           #extraigo el dato de las notas
+    Autor.objects.create(first_name = f'{dato_nombre_form}', last_name = f'{dato_apellido_form}', notas = f'{dato_notas_form}') #creo el libro en la base de datos
+    return redirect('/view_authors')
+
+def author_info(request, id_author):
+    abrir_autor = Autor.objects.get(id=id_author) #obtengo la informacion del autor
+    libros_autor = abrir_autor.libros.all() #obtengo los libros asociados al autor
+    lista_libros = Libro.objects.all().exclude(libros_autores=id_author)#recupero los libros y excluyo los libros que ya estan asociados
+    context = {
+        'info_autor' : abrir_autor,             #envio info del autor
+        'libros' : libros_autor,                  #envio info de los autores del libro
+        'libros_despliege' : lista_libros     #envio los libros restantes
+    }
+    return render(request, 'view_author.html', context)
+
+def asignando_libro(request, id_author):
+    id_libro = request.POST['id_libro']             #capturo numero id libro
+    libro_asig = Libro.objects.get(id=id_libro)     #obtengo el libro de db
+    autor_lib = Autor.objects.get(id=id_author)     #obtengo el autor de db
+    autor_lib.libros.add(libro_asig)                #ORM para asignar autor-libro
+    return redirect(f'/author_info/{id_author}')
