3437376da4e0904daa6df20682ebdc9f34c5a070
Hector Zambrano <hzambranoleal@gmail.com>
2021-03-17 00:59:21 -0300

    validaciones

--- django/django_full_stack/tv_show/show_app/models.py
+++ django/django_full_stack/tv_show/show_app/models.py
@@ -1,6 +1,33 @@
 from django.db import models
+from datetime import datetime
+print(datetime.today())
 
 # Create your models here.
+class TvManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title'])<2:
+            errors['title'] = 'title should be at least 2 characters!'
+
+        if len(postData['network'])<3:
+            errors['networ'] = 'networks should be at least 3 characters!'
+
+        if len(postData['release_date'])<8:
+            errors['release_date'] = 'enter a release date.'
+
+        if len(postData['description']) == 0:
+            errors['description'] = 'you have to write a description!'
+
+        if len(postData['description'])<10:
+            errors['description_10'] = 'description should be at least 10 characters!'
+
+
+        return errors
+
+
+
+
+
 class Tv_show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -8,3 +35,4 @@ class Tv_show(models.Model):
     description = models.TextField(default=000-00-00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = TvManager()
--- django/django_full_stack/tv_show/show_app/views.py
+++ django/django_full_stack/tv_show/show_app/views.py
@@ -1,5 +1,7 @@
-from show_app.models import Tv_show
+from show_app.models import *
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
 def new(request):
@@ -7,15 +9,22 @@ def new(request):
 
 
 def create(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    release_date = request.POST['release_date']
-    description = request.POST['description']
-    Tv_show.objects.create(title = f'{title}', network=f'{network}', release_date=f'{release_date}', description=f'{description}')
-    last_show = Tv_show.objects.last()
-    id = last_show.id
+    errors = Tv_show.objects.basic_validator(request.POST)
 
-    return redirect(f'/shows/{id}')
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        description = request.POST['description']
+        Tv_show.objects.create(title = f'{title}', network=f'{network}', release_date=f'{release_date}', description=f'{description}')
+        last_show = Tv_show.objects.last()
+        id = last_show.id
+
+        return redirect(f'/shows/{id}')
 
 
     #return HttpResponse('creacion en base de datos')
@@ -23,7 +32,7 @@ def create(request):
 def show_id(request, id):
     context = {
         'show': Tv_show.objects.get(id=id)
-        
+
     }
     return render(request, 'read_one.html', context)
     #return HttpResponse('muestra informacion del show')
@@ -43,15 +52,21 @@ def show_id_edit(request, id):
     #return HttpResponse('muestra la informacion para editar e id en link')
 
 def show_id_update(request, id):
+    errors = Tv_show.objects.basic_validator(request.POST)
 
-    update = Tv_show.objects.get(id=id)
-    update.title = request.POST['title']
-    update.network = request.POST['network']
-    update.release_date = request.POST['release_date']
-    update.description = request.POST['description']
-    update.save()
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{id}/edit')
+    else:
+        update = Tv_show.objects.get(id=id)
+        update.title = request.POST['title']
+        update.network = request.POST['network']
+        update.release_date = request.POST['release_date']
+        update.description = request.POST['description']
+        update.save()
 
-    return redirect(f'/shows/{id}')
+        return redirect(f'/shows/{id}')
     #return HttpResponse('para actualizar el registro en base de datos y redirige a /shows/id')
 
 def show_id_destroy(request, id):
